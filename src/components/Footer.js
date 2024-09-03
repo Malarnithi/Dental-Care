5217e9c531f08f6fc23856616e67436c29f0e916
Malarnithi <malarnithivelusamy21@gmail.com>
2024-09-03 16:08:04 +0530

    correction

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
-// Footer.js
 import React from 'react';
 import arrow from './Frame 265.png';
-import social from './Frame 269.png';
 import facebook from './facebook.png';
 import twitter from './twitter.png';
 import youtube from './youtube.png';
@@ -10,56 +8,56 @@ import linkedin from './linkedin.png';
 import tiktok from './tiktok.png';
 import instagram from './instagram.png';
 
-
 export default function Footer() {
   return (
     <div className='footer'>
-          <span className='footer-brand' href="/">57<span style={{color:'#89DB7B'}}>D</span><span>entcare</span></span>
-          <div className='footer-content'>
-            <div class="footer1">
-               <h3>Company</h3>
-               <p>Home</p>
-               <p>Services</p>
-               <p>About Us</p>
-               <p>Why Choose Us</p>
-               <p>Testimonials</p>
-               <p>Contact Us</p>
-           </div>
-            <div class="footer2"> 
-            <h3>Pages</h3>
-               <p>404</p>
-               <p>Licensing</p>
-               <p>Instructions</p>
-               <p>Style Guide</p>
-               <p>Changelog</p>
-               <p>More Templates!</p>
-            </div>
-            <div class="footer3"> 
-            <h3>Address</h3>
-            <p>123 Dental Avenue City ville,</p>
-            <p> State 12345 United States</p>
-            <p>View on Maps</p>
+      <span className='footer-brand' href="/">57<span style={{color:'#89DB7B'}}>D</span><span>entcare</span></span>
+      <div className='footer-content'>
+        <div className="footer1">
+          <h3>Company</h3>
+          <p>Home</p>
+          <p>Services</p>
+          <p>About Us</p>
+          <p>Why Choose Us</p>
+          <p>Testimonials</p>
+          <p>Contact Us</p>
+        </div>
+        <div className="footer2"> 
+          <h3>Pages</h3>
+          <p>404</p>
+          <p>Licensing</p>
+          <p>Instructions</p>
+          <p>Style Guide</p>
+          <p>Changelog</p>
+          <p>More Templates!</p>
+        </div>
+        <div className="footer3"> 
+          <h3>Address</h3>
+          <p>123 Dental Avenue Cityville,</p>
+          <p>State 12345 United States</p>
+          <p>View on Maps</p>
 
-            <h3>Enquiries</h3>
-            <p>(123) 456-7890</p>
-            <p>info@57dentcare.com</p>
-            </div>
-            <div class="footer4">
-            <h3>Newsletter</h3>
-            <p>Stay Updated with our Latest News</p>
-            <input className='newsletter' placeholder='Your Email'></input><img src={arrow} alt="arrow" />
-             <div className='social-icons'>
-              <h5>Follow Us</h5>
-              <img src={facebook} width='24px'alt='facebook'/>
-              <img src={twitter} width='22px' alt='twitter'/>
-              <img src={youtube} width='22px' alt='youtube' />
-              <img src={pinterest} width='22px' alt='pinterest'/>
-              <img src={linkedin} width='20px' alt='linkedin'/>
-              <img src={tiktok} width='20px' alt='tiktok'/>
-              <img src={instagram} width='20px' alt='instagram'/>
-              </div> 
-             </div>
-          </div>
+          <h3>Enquiries</h3>
+          <p>(123) 456-7890</p>
+          <p>info@57dentcare.com</p>
+        </div>
+        <div className="footer4">
+          <h3>Newsletter</h3>
+          <p>Stay Updated with our Latest News</p>
+          <input className='newsletter' placeholder='Your Email' />
+          <img src={arrow} alt="Arrow icon" />
+          <div className='social-icons'>
+            <h5>Follow Us</h5>
+            <img src={facebook} width='24px' alt='Facebook' />
+            <img src={twitter} width='22px' alt='Twitter' />
+            <img src={youtube} width='22px' alt='YouTube' />
+            <img src={pinterest} width='22px' alt='Pinterest' />
+            <img src={linkedin} width='20px' alt='LinkedIn' />
+            <img src={tiktok} width='20px' alt='TikTok' />
+            <img src={instagram} width='20px' alt='Instagram' />
+          </div> 
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Service.js
+++ src/components/Service.js
@@ -9,76 +9,74 @@ import image6 from './image6.png';
 
 export default function Service() {
   return (
-    <>
-      <div className='service'>
-        <div className='service-head'>
-          <h4 className='head-color'>Satisfy Solution</h4>
-          <h1>The Best Dental Treatment</h1>
+    <div className='service'>
+      <div className='service-head'>
+        <h4 className='head-color'>Satisfy Solution</h4>
+        <h1>The Best Dental Treatment</h1>
+      </div>
+      <div className='service-content'>
+        <div className="service1">
+          <div className='Service-text'>
+            <h4>Teeth Whitening</h4>
+            <p>Cosmetic treatment to enhance the whiteness of teeth</p>
+            <button className='Readmore'>Read More</button>
+          </div>
+          <div className='Service-image'>
+            <img src={image11} width='70px' height='200px' alt="Teeth whitening process image 1" />
+            <img src={image1} width='40px' height='200px' alt="Teeth whitening process image 2" />
+          </div>
         </div>
-        <div className='service-content'>
-          <div className="service1">
-            <div className='Service-text'>
-              <h4>Teeth Whitening</h4>
-              <p>Cosmetic treatment to enhance the whiteness of teeth</p>
-              <button className='Readmore'>Read More</button>
-            </div>
-            <div className='Service-image'>
-              <img src={image11} width='70px' height='200px' alt="Teeth Whitening Process Image 1" />
-              <img src={image1} width='40px' height='200px' alt="Teeth Whitening Process Image 2" />
-            </div>
+        <div className="service2"> 
+          <div className='Service-text'>
+            <h4>Dental Implant</h4>
+            <p>Surgical placement of artificial tooth roots</p>
+            <button className='Readmore'>Read More</button>
           </div>
-          <div className="service2"> 
-            <div className='Service-text'>
-              <h4>Dental Implant</h4>
-              <p>Surgical placement of artificial tooth roots</p>
-              <button className='Readmore'>Read More</button>
-            </div>
-            <div className='Service-image'>
-              <img src={image2} height='200px' alt="Dental Implant Procedure Image" />
-            </div>
+          <div className='Service-image'>
+            <img src={image2} height='200px' alt="Dental implant procedure" />
           </div>
-          <div className="service3">
-            <div className='Service-text'>
-              <h4>Teeth Filling</h4>
-              <p>Restorative procedures to repair cavities and damaged teeth</p> 
-              <button className='Readmore'>Read More</button>
-            </div>
-            <div className='Service-image'>
-              <img src={image3} height='200px' alt="Teeth Filling Process Image" />
-            </div>
+        </div>
+        <div className="service3">
+          <div className='Service-text'>
+            <h4>Teeth Filling</h4>
+            <p>Restorative procedures to repair cavities and damaged teeth</p> 
+            <button className='Readmore'>Read More</button>
           </div>
-          <div className="service4">
-            <div className='Service-text'>
-              <h4>Oral Surgery</h4>
-              <p>Surgical procedures, including extractions and dental implants</p>
-              <button className='Readmore'>Read More</button>
-            </div>
-            <div className='Service-image'>
-              <img src={image4} height='200px' alt="Oral Surgery Procedure Image" />
-            </div> 
+          <div className='Service-image'>
+            <img src={image3} height='200px' alt="Teeth filling process" />
           </div>
-          <div className="service5"> 
-            <div className='Service-text'>
-              <h4>Crown and Bridges</h4>
-              <p>Restoration options for damaged or missing teeth</p>
-              <button className='Readmore'>Read More</button>
-            </div>
-            <div className='Service-image'>
-              <img src={image5} height='200px' alt="Crown and Bridges Procedure Image" />
-            </div>
+        </div>
+        <div className="service4">
+          <div className='Service-text'>
+            <h4>Oral Surgery</h4>
+            <p>Surgical procedures, including extractions and dental implants</p>
+            <button className='Readmore'>Read More</button>
           </div>
-          <div className="service6">
-            <div className='Service-text'>
-              <h4>Periodontal Care</h4>
-              <p>Services for gum health, including scaling and root planing</p>
-              <button className='Readmore'>Read More</button>
-            </div>
-            <div className='Service-image'>
-              <img src={image6} height='200px' alt="Periodontal Care Procedure Image" />
-            </div> 
+          <div className='Service-image'>
+            <img src={image4} height='200px' alt="Oral surgery procedure" />
+          </div> 
+        </div>
+        <div className="service5"> 
+          <div className='Service-text'>
+            <h4>Crown and Bridges</h4>
+            <p>Restoration options for damaged or missing teeth</p>
+            <button className='Readmore'>Read More</button>
           </div>
+          <div className='Service-image'>
+            <img src={image5} height='200px' alt="Crown and bridges procedure" />
+          </div>
+        </div>
+        <div className="service6">
+          <div className='Service-text'>
+            <h4>Periodontal Care</h4>
+            <p>Services for gum health, including scaling and root planing</p>
+            <button className='Readmore'>Read More</button>
+          </div>
+          <div className='Service-image'>
+            <img src={image6} height='200px' alt="Periodontal care procedure" />
+          </div> 
         </div>
       </div>
-    </>
+    </div>
   );
 }
